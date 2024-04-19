cf713c20836a50cced522d82668cb1c7fe8e86c9
Haylie Tan <141064325+haylietan@users.noreply.github.com>
2024-04-19 09:26:03 -0700

    Updated teams page

---
+++ my-app/src/Components/individual components/member.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../../Styles/team.css';
+
+export const Card = () => {
+    return (
+        <div className="child">
+
+            <div className="childtop">
+            [img]
+            </div>
+
+            <div className="childbottom">
+                <h2>First Last</h2>
+                <p>Position | Major, Year</p>
+            </div>
+
+        </div>
+
+    )
+}
--- my-app/src/Components/team.js
+++ my-app/src/Components/team.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import '../App.css';
 import '../Styles/team.css';
+import {Card} from "./individual components/member";
 
 export const Team = () => {
     return (
         <div> 
-            <p> hiiii </p>
+            <div className="intro">
+                <h1> Meet Us </h1>
+                <p>UC Davis students wanting to make a change.</p>
+                <div>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                </div>
+            </div>
         </div>
     )
 }
