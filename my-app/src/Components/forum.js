231f9f094ff047e8423de607b359d370b3c11add
Haylie Tan <141064325+haylietan@users.noreply.github.com>
2024-05-03 06:54:30 -0700

    Added search bar to forum

--- my-app/src/Components/forum.js
+++ my-app/src/Components/forum.js
@@ -5,8 +5,24 @@ import '../Styles/forum.css';
 
 export const Forum = () => {
     return (
-        <div className="forum-intro">
-            <h1>Forum</h1>
+        <div>
+            <div className="forum-intro">
+                <h1>Forum</h1>
+
+                <div className="forum-search-bar">
+                <input type="text" placeholder="Search here"></input>
+                <button>
+                    <a href="#">Search</a>
+                    <ion-icon name="search-outline" class="search-icon"></ion-icon>
+                </button>
+                
+            </div>
+            </div>
+
+            <div className='parent'>
+                <div className='child'>child 1</div>
+                <div className='child'>child 2</div>
+            </div>
         </div>
     )
 }
