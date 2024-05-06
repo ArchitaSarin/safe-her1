5ffdbdd8b505953ca9c8c3cb398619ae1ba7c725
Haylie Tan <141064325+haylietan@users.noreply.github.com>
2024-05-06 15:41:59 -0700

    Updated teams page

--- my-app/src/Components/team.js
+++ my-app/src/Components/team.js
@@ -8,10 +8,12 @@ export const Team = () => {
         <div> 
             <div className="about-us-team">
                 <h1> About Us </h1>
-                <p> blah blah </p>
+                <p>
+                At SafeHER, our mission is to create a digital safe community and platform for women on the UC Davis campus. We believe that every woman deserves to feel safe, supported, and empowered, and we are committed to providing resources, support, and a sense of community for all women at UC Davis.
+                </p>
             </div>
             <div className="intro">
-                <h1> Meet Us </h1>
+                <h1> Meet the Team </h1>
                 <p>UC Davis students wanting to make a change.</p>
                 
                 <div>
@@ -28,7 +30,12 @@ export const Team = () => {
             </div>
 
             <div className="team_impact">
-                
+                <h1> Our Values </h1>
+                <div className="pillars-parent">
+                    <div className="pillars-child">child</div>
+                    <div className="pillars-child">child</div>
+                    <div className="pillars-child">child</div>
+                </div>
             </div>
         </div>
     )
