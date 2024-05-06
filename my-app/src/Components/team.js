e24d3a0cf5fe9e8b0375c1e819ee93aaa7c5421c
Haylie Tan <141064325+haylietan@users.noreply.github.com>
2024-05-06 15:28:29 -0700

    Updated teams page

--- my-app/src/Components/team.js
+++ my-app/src/Components/team.js
@@ -6,6 +6,10 @@ import {Card} from "./individual components/member";
 export const Team = () => {
     return (
         <div> 
+            <div className="about-us-team">
+                <h1> About Us </h1>
+                <p> blah blah </p>
+            </div>
             <div className="intro">
                 <h1> Meet Us </h1>
                 <p>UC Davis students wanting to make a change.</p>
