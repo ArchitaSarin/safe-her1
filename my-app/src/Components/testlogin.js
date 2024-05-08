6a4ee614225ba73559a55f09cb6e44f82eadc7dc
anvikalucha <anvikalucha@gmail.com>
2024-05-08 14:02:02 -0700

    Implemented Google Login for frontend React

--- my-app/src/Components/testlogin.js
+++ my-app/src/Components/testlogin.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import '../Styles/login.css';
+import { GoogleLogin } from '@react-oauth/google';
 
 export const TestLogin = () => {
+    const responseMessage = (response) => {
+        console.log(response);
+    };
+    const errorMessage = (error) => {
+        console.log(error);
+    };
     return (
         <div className='height'>
-            <p>hi</p>
+            <h2>React Google Login</h2>
+            <br />
+            <br />
+            <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
         </div>
     )
 }
+
+// function TestLogin() {
+//     const responseMessage = (response) => {
+//         console.log(response);
+//     };
+//     const errorMessage = (error) => {
+//         console.log(error);
+//     };
+//     return (
+//         <div>
+//             <h2>React Google Login</h2>
+//             <br />
+//             <br />
+//             <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+//         </div>
+//     )
+// }
+// export default TestLogin;
