a0ef5f8f271ccb87f8d39df2c7b4dd58fb85f158
Kylie Lallak <140771878+Kylie-Lallak@users.noreply.github.com>
2024-05-19 18:19:56 -0700

    heart svg upload

--- my-app/src/Components/resources.js
+++ my-app/src/Components/resources.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import '../App.css';
 import '../Styles/resources.css';
-import '../Images/resources-header.png'
+import HeaderImage from '../Images/resources-header.png'
+
 
 export const Resources = () => {
     return (
         <div id="resources-header">
-            <h1 id="header-main-text">Resources</h1>
+            <div id="header-text">
+                <h1 id="header-main-text">Resources</h1>
+                <p id="header-small-text">We want you to be safe, happy and heard</p>
+
+            </div>
+        
+          
 
 
         </div>
