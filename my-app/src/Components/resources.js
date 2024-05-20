5ec0c0e362f648f17b81d46760401af62b115da0
Kylie Lallak <140771878+Kylie-Lallak@users.noreply.github.com>
2024-05-19 18:32:16 -0700

    heart svg small size upload

--- my-app/src/Components/resources.js
+++ my-app/src/Components/resources.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../App.css';
 import '../Styles/resources.css';
 import HeaderImage from '../Images/resources-header.png'
+import Heart from '../Images/heart-regular.svg'
 
 
 export const Resources = () => {
@@ -9,7 +10,7 @@ export const Resources = () => {
         <div id="resources-header">
             <div id="header-text">
                 <h1 id="header-main-text">Resources</h1>
-                <p id="header-small-text">We want you to be safe, happy and heard</p>
+                <p id="header-small-text"> <img src={Heart} class="heart"/>We want you to be safe, happy and heard</p>
 
             </div>
         
