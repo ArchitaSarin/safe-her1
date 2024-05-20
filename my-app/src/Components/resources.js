0c5867ad69e18116a660450bddb3c00cb49475c9
Kylie Lallak <140771878+Kylie-Lallak@users.noreply.github.com>
2024-05-19 23:19:49 -0700

    more svg file uploads

--- my-app/src/Components/resources.js
+++ my-app/src/Components/resources.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../App.css';
 import '../Styles/resources.css';
 import HeaderImage from '../Images/resources-header.png'
-import Heart from '../Images/heart-small.svg'
+import Heart from '../Images/heart.svg'
 
 
 export const Resources = () => {
@@ -10,7 +10,27 @@ export const Resources = () => {
         <div id="resources-header">
             <div id="header-text">
                 <h1 id="header-main-text">Resources</h1>
-                <p id="header-small-text"> <img src={Heart} class="heart"/>We want you to be safe, happy and heard</p>
+                <p id="header-small-text"> <img src={Heart} class="heart"/>We want you to be safe, happy and heard <img src={Heart} class="heart"/></p>
+
+            </div>
+
+            <div id="header-box-container">
+                <div class="header-box">
+                    <div class="header-box-text">
+                        <h2>Confidental 24/7 Support</h2>
+                        <h3>National Sexual Assault Hotline</h3>
+                        <p>Languages: English, Spainish</p>
+                    
+                    </div>
+
+                    <div class="header-box-button">
+                        <p class="box-phone">(800) 656-4673</p>
+
+                    </div>
+                    
+
+
+                </div>
 
             </div>
         
