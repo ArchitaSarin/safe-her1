bfd6fcaf3fa9a94fcb64becf196f4d0cd034a913
Kylie Lallak <140771878+Kylie-Lallak@users.noreply.github.com>
2024-05-20 00:15:39 -0700

    more svg file edits

--- my-app/src/Components/resources.js
+++ my-app/src/Components/resources.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import '../App.css';
 import '../Styles/resources.css';
-import HeaderImage from '../Images/resources-header.png'
-import Heart from '../Images/heart.svg'
+import HeaderImage from '../Images/resources-header.png';
+import Heart from '../Images/heart.svg';
+import Phone from '../Images/phone.svg';
+
+
 
 
 export const Resources = () => {
     return (
+       
         <div id="resources-header">
             <div id="header-text">
                 <h1 id="header-main-text">Resources</h1>
                 <p id="header-small-text"> <img src={Heart} class="heart"/>We want you to be safe, happy and heard <img src={Heart} class="heart"/></p>
 
             </div>
-
+            <img src={Phone}/>
             <div id="header-box-container">
                 <div class="header-box">
                     <div class="header-box-text">
-                        <h2>Confidental 24/7 Support</h2>
+                        <h2> Confidental 24/7 Support</h2>
                         <h3>National Sexual Assault Hotline</h3>
                         <p>Languages: English, Spainish</p>
                     
