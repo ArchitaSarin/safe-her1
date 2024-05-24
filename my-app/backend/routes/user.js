08703e584d8d8c8029d581fd3cc0a854dd827f56
anvikalucha <anvikalucha@gmail.com>
2024-05-23 22:25:09 -0700

    finished user routes

---
+++ my-app/backend/routes/user.js
@@ -0,0 +1,59 @@
+const router = require("express").Router();
+const { restart } = require("nodemon");
+const User = require("../models/user.model.js");
+
+router.route("/").get((req, res) => {
+    res.send("hello world :D");
+})
+
+router.route("/add").post((req, res) => {
+    const email = req.body.email;
+    const name = req.body.name;
+    const user_id = req.body.user_id;
+    // const picture = req.picture;
+    // const verified_email = req.body.verified_email;
+
+    const newItem = new User({
+        email: email,
+        name: name,
+        user_id: user_id,
+        // picture: picture,
+        // verified_email
+    });
+
+    newItem
+        .save()
+        .then(() => res.json(" New Item Added!"))
+        .catch((err) => res.status(400).json("Error: " + err));
+});
+
+
+router.route("/get/:name").get((req, res) => {
+    User.find({ "name": req.params.name })
+        .then(user => res.json(user))
+        .catch(err => res.status(400).json("Error: " + err));
+});
+
+router.route("/get/:email").get((req, res) => {
+    User.find({ "email": req.params.email })
+        .then(user => res.json(user))
+        .catch(err => res.status(400).json("Error: " + err));
+});
+
+router.route("/update/:name").put((req, res) => {
+    User.findOne({ name: req.params.name })
+        .then(item => {
+            item.name = req.body.name;
+            item.save()
+                .then(() => res.status(200).json("Item Updated!"))
+                .catch(err => res.status(400).json("Error: " + err));
+        });
+});
+
+router.route("/delete/:name").delete((req, res) => {
+    User.findOneAndDelete({ name: req.params.name })
+        .then(item => res.json(item))
+        .catch(err => res.status(400).json("Error: " + err));
+});
+
+module.exports = router;
---
+++ my-app/backend/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5001;
+
+app.use(
+    express.urlencoded({ extended: true })
+);
+    
+app.use(express.json());
+
+const cors = require("cors");
+app.use(cors());
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', function(req, res, next) {
+    res.send("Hello world");
+});
+
+const userRouter = require("./routes/user");
+app.use("/user", userRouter);
+
+app.listen(port, () => {
+    console.log(`⚡[server]: Server is running at http://localhost:${port}`);
+});
+
+const uri = process.env.ATLAS_URI; // load mongodb key
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(uri);
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("MongoDB database connection established successfully.");
+});
+
