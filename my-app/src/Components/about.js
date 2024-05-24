a73aec7dbe9fead8abea6764da226ad08380af31
Haylie Tan <141064325+haylietan@users.noreply.github.com>
2024-05-23 18:23:31 -0700

    Finished home page

--- my-app/src/Components/about.js
+++ my-app/src/Components/about.js
@@ -20,6 +20,9 @@ export const About = () => {
                 <h1><u>♡ community...</u></h1>
             </div>
 
+            <div className="container-description">
+                <h2>that cultivates an inclusive sanctuary</h2>
+            </div>
             <div className="pink-landing-container">
                 <div className="pink-rect-text">
                     <h2>Resouces</h2>
@@ -28,7 +31,60 @@ export const About = () => {
                     </p>
                     <btn className="rect-button">Learn More</btn>
                 </div>
+
+                <div className="pink-rect-image">
+                </div>
             </div>
+
+            <div className="container-description">
+                <h2>where women can freely share their creativity</h2>
+            </div>
+            <div className="purple-landing-container">
+                <div className="purple-rect-image">
+                </div>
+
+                <div className="purple-rect-text">
+                    <h2>Art Board</h2>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida felis at mi scelerisque, ac eleifend lacus consequat. Nulla facilisi.
+                    </p>
+                    <btn className="rect-button">Learn More</btn>
+                </div>
+            </div>
+
+            <div className="container-description">
+                <h2>and amplify their voices</h2>
+            </div>
+            <div className="red-landing-container">
+                <div className="red-rect-text">
+                    <h2>Blog</h2>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida felis at mi scelerisque, ac eleifend lacus consequat. Nulla facilisi.
+                    </p>
+                    <btn className="rect-button">Learn More</btn>
+                </div>
+
+                <div className="red-rect-image">
+                </div>
+            </div>
+
+            <div className="container-description">
+                <h2>and feel heard.</h2>
+            </div>
+            <div className="green-landing-container">
+                <div className="green-rect-image">
+                </div>
+
+                <div className="green-rect-text">
+                    <h2>Rant Bot</h2>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida felis at mi scelerisque, ac eleifend lacus consequat. Nulla facilisi.
+                    </p>
+                    <btn className="rect-button">Learn More</btn>
+                </div>
+            </div>
+
+            
 
         </div>
     )
